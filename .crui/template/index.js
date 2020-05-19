f0b49ac710b1b66977128397a6fdbe759d553c40
rywaroy <464485682@qq.com>
2020-05-19 15:42:43 +0800

    Feat

---
+++ .crui/template/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Template extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>自定义模板文件</div>;
+  }
+}
+
+export default Template;
